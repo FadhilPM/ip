fe5146e435f1f2d6efd1e3dbb9f8ee64120cea8a
Fadhil <e0148630@u.nus.edu>
2025-01-21 15:46:38 +0800

    Initial commit: Renamed bot and added skeletal methods.

---
+++ src/main/java/Bezdelnik.java
@@ -0,0 +1,25 @@
+public class Bezdelnik {
+    private static final String divider = "_".repeat(100);
+    private static final String logo = " _____   _____   _____     _____     _____    ____    _       _   _   _     _   _   _ \n"
+                   + "|  ___| |  ___| |___  |   |  _  |   |  ___|  |  _ \\  | |     | | | | | |   / | | | / / \n"
+                  + "| |___  | |___     _| |   | | | |   | |___   | | | | | |___  | |_| | | |  // | | |/ /\n"
+                  + "|  _  | |  ___|   |_  |  _| |_| |_  |  ___|  | | | | |  _  | |  _  | | | //| | |   (\n"
+                  + "| |_| | | |___   ___| | |  _____  | | |___   | | | | | |_| | | | | | | |// | | | |\\ \\\n"
+                  + "|_____| |_____| |_____| |_|     |_| |_____|  /_/ |_| |_____| |_| |_| |_ /  |_| |_| \\_\\ ";
+    public static void main(String[] args) {
+        String greeting = String.format("%s\nHello from\n%s\n\nWhat can I do for you?\n%s",
+                "_".repeat(104), logo, "_".repeat(104));
+        System.out.println(greeting);
+        inputLoop();
+        System.out.println(responseFormat("Bye. Hope to see you again soon!"));
+    }
+
+    private static void inputLoop() {
+
+    }
+
+    private static String responseFormat(String input) {
+        String output = "    " + divider + "\n" + "    " + input + "\n" + "    " + divider;
+        return output;
+    }
+}
