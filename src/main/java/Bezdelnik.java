8c650dbcc8d96cdeb23a7a98a8e4ebd0ff83fa03
Fadhil <e0148630@u.nus.edu>
2025-01-21 16:33:20 +0800

    Added storage of text by user display of stored text on demand.

--- src/main/java/Bezdelnik.java
+++ src/main/java/Bezdelnik.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.stream.IntStream;
 
 public class Bezdelnik {
+    private static List<Task> taskList = new ArrayList<Task>();
     private static final String divider = "_".repeat(100);
     private static final String logo = " _____   _____   _____     _____     _____    ____    _       _   _   _     _   _   _ \n"
                    + "|  ___| |  ___| |___  |   |  _  |   |  ___|  |  _ \\  | |     | | | | | |   / | | | / / \n"
@@ -8,12 +12,13 @@ public class Bezdelnik {
                   + "|  _  | |  ___|   |_  |  _| |_| |_  |  ___|  | | | | |  _  | |  _  | | | //| | |   (\n"
                   + "| |_| | | |___   ___| | |  _____  | | |___   | | | | | |_| | | | | | | |// | | | |\\ \\\n"
                   + "|_____| |_____| |_____| |_|     |_| |_____|  /_/ |_| |_____| |_| |_| |_ /  |_| |_| \\_\\ ";
+
     public static void main(String[] args) {
         String greeting = String.format("%s\nHello from\n%s\n\nWhat can I do for you?\n%s",
                 "_".repeat(104), logo, "_".repeat(104));
         System.out.println(greeting);
         inputLoop();
-        System.out.println(responseFormat("Bye. Hope to see you again soon!"));
+        System.out.println(responseFormat("\tBye. Hope to see you again soon!"));
     }
 
     private static void inputLoop() {
@@ -23,19 +28,26 @@ public class Bezdelnik {
             input = sc.nextLine();
         }
         sc.close();
-
     }
 
     private static boolean stringParser(String input) {
-        if (!input.equals("bye")) {
-            System.out.println(responseFormat(input));
+        if (input.equals("bye")) {
+            return false;
+        } else if (input.equals("list")) {
+            String output = IntStream.range(0, taskList.size())
+                .mapToObj(x -> String.format("\t%d. %s", x + 1, taskList.get(x).toString()))
+                .reduce((a, b) -> a + "\n" + b)
+                .orElse("No tasks present!");
+            System.out.println(responseFormat(output));
             return true;
         }
-        return false;
+        Task toAdd = new Task(input);
+        taskList.add(toAdd);
+        System.out.println(responseFormat("\tadded: " + input));
+        return true;
     }
 
     private static String responseFormat(String input) {
-        String output = "    " + divider + "\n" + "    " + input + "\n" + "    " + divider;
-        return output;
+        return String.format("\t%s\n%s\n\t%s", divider, input, divider);
     }
 }
