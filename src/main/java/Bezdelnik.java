551782586ce416722883e8c46900d0df33af801d
Fadhil <e0148630@u.nus.edu>
2025-01-23 02:25:34 +0800

    Refactor
    Moved string parser to separate class.

--- src/main/java/Bezdelnik.java
+++ src/main/java/Bezdelnik.java
@@ -26,7 +26,7 @@ public class Bezdelnik {
             .tokens()
             .map(input -> input.strip())
             .takeWhile(input -> !input.matches("(bye|(/)?ex(it)?)"))
-            .map(input -> stringParser(input, taskList))
+            .map(input -> CommandParser.parse(input, taskList))
             .forEach(pair ->  {
                     System.out.println(responseFormat(pair.first()));
                     taskList = pair.second();
@@ -34,69 +34,6 @@ public class Bezdelnik {
             );
         sc.close();
     }
-
-    //should probably be its own class...
-    //incredibly ugly string parsing within
-    private static Pair<String, Taskman> stringParser(String input, Taskman taskman) {
-        String toReturn;
-        Taskman newTaskman = taskman;
-        try {
-            switch (input.split(" ")[0]) {
-                case "list", "ls" -> {
-                    toReturn = taskList.toString();
-                }
-                case "mark" -> {
-                    int i = Integer.parseUnsignedInt(input.split(" ")[1]) - 1;
-                    newTaskman = newTaskman.operate(i, x -> x.markAsDone());
-                    toReturn = String.format("\tI have marked this task as done.\n\t%s", newTaskman.get(i).toString());
-                }
-                case "unmark" -> {
-                    int i = Integer.parseUnsignedInt(input.split(" ")[1]) - 1;
-                    newTaskman = newTaskman.operate(i, x -> x.markAsUndone());
-                    toReturn = String.format("\tI have marked this task as undone.\n\t%s", newTaskman.get(i).toString());
-                }
-                case "delete" -> {
-                    int i = Integer.parseUnsignedInt(input.split(" ")[1]) - 1;
-                    Task toDelete = newTaskman.get(i);
-                    newTaskman = newTaskman.remove(i);
-                    toReturn = String.format("\tI have deleted this task.\n\t%s", toDelete.toString());
-                }
-                case "todo" -> {
-                    String todoInput = input.substring(5);
-                    if (todoInput.isEmpty()) {
-                        toReturn = "\ttodo must be followed with something to do!";
-                    } else {
-                        Task toAdd = new Todo(todoInput);
-                        newTaskman = newTaskman.add(toAdd);
-                        toReturn = String.format("\tadded:\n\t%s\n\tYou currently have %d task(s)", toAdd.toString(), newTaskman.size());
-                    }
-                }
-                case "deadline" -> {
-                    String deadlineInput = input.substring(9);
-                    String[] array = deadlineInput.split(" /by ");
-                    Task toAdd = new Deadline(array[0], array[1]);
-                    newTaskman = newTaskman.add(toAdd);
-                    toReturn = String.format("\tadded:\n\t%s\n\tYou currently have %d task(s)", toAdd.toString(), newTaskman.size());
-                }
-                case "event" -> {
-                    String eventInput = input.substring(6);
-                    String[] array = eventInput.split(" /");
-                    Task toAdd = new Event(array[0], array[1].substring(5), array[2].substring(3));
-                    newTaskman = newTaskman.add(toAdd);
-                    toReturn = String.format("\tadded:\n\t%s\n\tYou currently have %d task(s)", toAdd.toString(), newTaskman.size());
-                }
-                default -> {
-                    toReturn = String.format("\tUnsupported command: %s", input);
-                }
-            }
-        } catch (NumberFormatException n) {
-            toReturn = "\tI'm sorry, that was not a valid integer you specified. Try again!";
-        } catch (IndexOutOfBoundsException i) {
-            toReturn = String.format("\tInvalid command parameters!");
-        }
-        return new Pair<String, Taskman>(toReturn, newTaskman);
-    }
-
 
     private static String responseFormat(String input) {
         return String.format("\t%s\n%s\n\t%s", divider, input, divider);
--- src/main/java/Pair.java
+++
@@ -1,2 +0,0 @@
-// Will be useful when I spin the parser off into its own class.
-public record Pair<T, U>(T first, U second) {};
