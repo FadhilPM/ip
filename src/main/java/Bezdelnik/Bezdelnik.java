e11520a3fefb745aa942dfbcc99c2e498a055d5b
Fadhil <e0148630@u.nus.edu>
2025-02-01 20:47:22 +0800

    Create UI utility class

--- src/main/java/Bezdelnik/Bezdelnik.java
+++ src/main/java/Bezdelnik/Bezdelnik.java
@@ -1,54 +1,37 @@
 package Bezdelnik;
 
-import java.util.Scanner;
-
 /**
  * Main entry point to chatbot
  *
  */
 public class Bezdelnik {
     private static Taskman taskman = new Taskman();
-    private static final String divider = "_".repeat(100);
-    private static final String logo =
-        " _____   _____   _____     _____     _____    ____    _       _   _   _     _   _   _ \n"
-        + "|  ___| |  ___| |___  |   |  _  |   |  ___|  |  _ \\  | |     | | | | | |   / | | | / / \n"
-        + "| |___  | |___     _| |   | | | |   | |___   | | | | | |___  | |_| | | |  // | | |/ /\n"
-        + "|  _  | |  ___|   |_  |  _| |_| |_  |  ___|  | | | | |  _  | |  _  | | | //| | |   (\n"
-        + "| |_| | | |___   ___| | |  _____  | | |___   | | | | | |_| | | | | | | |// | | | |\\ \\\n"
-        + "|_____| |_____| |_____| |_|     |_| |_____|  /_/ |_| |_____| |_| |_| |_ /  |_| |_| \\_\\ ";
-    private static final String greeting = String.format("%s\nHello from\n%s\n\nWhat can I do for you?\n%s",
-                "_".repeat(104), logo, "_".repeat(104));
     private static final String saveLocation = "./data/output.dat";
 
     public static void main(String[] args) {
-        System.out.println(greeting);
-        taskman = Storage.readTaskmanFromFile(saveLocation);
-        inputLoop();
-        System.out.println(responseFormat("\tBye. Hope to see you again soon!"));
+        Pair<String, Taskman> readAttempt;
+        try {
+            readAttempt = Storage.readTaskmanFromFile(saveLocation);
+        } catch (Throwable e) {
+            readAttempt = new Pair<String, Taskman>("        No prior data found, creating new session", new Taskman());
+        }
+        inputLoop(readAttempt.first());
     }
 
-    private static void inputLoop() {
-        Scanner sc = new Scanner(System.in);
-        sc.useDelimiter("\n")
-            .tokens()
-            .map(input -> input.strip())
-            .takeWhile(input -> !input.matches("(bye|(/)?ex(it)?)"))
+    private static void inputLoop(String sessionStatus) {
+        Ui.greet(sessionStatus);
+        Ui.takeInput()
             .map(input -> Parser.parse(input, taskman))
             .forEach(pair -> {
-                        System.out.println(responseFormat(pair.first()));
+                        Ui.print(pair.first());
                         taskman = pair.second();
-                        Storage.writeTaskmanToFile(taskman, saveLocation);
+                        try {
+                            Storage.writeTaskmanToFile(taskman, saveLocation);
+                        } catch (Throwable e) {
+                            Ui.print(String.format("Unknown exception when saving data.", e.toString()));
+                        }
                     }
             );
-        sc.close();
-    }
-
-    private static String responseFormat(String input) {
-        return String.format("\t%s\n%s\n\t%s", divider, input, divider);
-    }
-
-    @Override
-    public String toString() {
-        return String.format("%s%s", "Bezdelnik\n", taskman.toString());
+        Ui.bye();
     }
 }
