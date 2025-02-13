7e594991eca316e9f657a5b11010336d0ffd500e
Fadhil <e0148630@u.nus.edu>
2025-02-13 12:58:59 +0800

    Add checkstyle config files

--- src/main/java/bezdelnik/Launcher.java
+++ src/main/java/bezdelnik/Launcher.java
@@ -2,6 +2,9 @@ package bezdelnik;
 
 import javafx.application.Application;
 
+/*
+ * Main entry point for GUI task manager
+ */
 public class Launcher {
     public static void main(String[] args) {
         Application.launch(Main.class, args);
--- src/main/java/bezdelnik/Main.java
+++ src/main/java/bezdelnik/Main.java
@@ -22,8 +22,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
-            //bezdelnik.initialise();
-            fxmlLoader.<MainWindow>getController().setBezdelnik(bezdelnik);  // inject the Duke instance
+            fxmlLoader.<MainWindow>getController().setBezdelnik(bezdelnik);
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
